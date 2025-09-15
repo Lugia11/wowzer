34d3f0f1d1fb14dfb9ac96679e28549c048938ce
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-15 08:34:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyscript import document
- from pip install keyboard import keyboard
+ from https://pypi.org/project/keyboard/ import keyboard
 
 def on_w_press(event):
     if event.name == 'w':
@@ -11,5 +11,6 @@ print ("Hello world")
 output_div = document.querySelector("#textarea")
 
 output_div.innerText = "hello world!"
+
 
 
