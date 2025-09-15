dfcded611ba1472ea7fd28f4b39cce8e9982dbb2
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-15 08:23:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from pyscript import document
 import keyboard
-if keyboard.is_pressed ("w")
-  output_div.innerText = "w is pressed"
+
+def on_w_press(event):
+    if event.name == 'w':
+        print("w is pressed")
+        # If you want to execute JS code, you'd need a web context.
+
+keyboard.on_press(on_w_press)
 print ("Hello world")
 output_div = document.querySelector("#textarea")
 
 output_div.innerText = "hello world!"
+
