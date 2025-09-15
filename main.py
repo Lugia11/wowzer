c21df5e8dcd082559ef3da0fb0cc0badb0ff1e89
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-15 08:17:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from pyscript import document
-print ("hello world!")
+import keyboard
+if keyboard.is_pressed ("w")
+  output_div.innerText = "w is pressed"
+print ("Hello world")
 output_div = document.querySelector("#textarea")
+
 output_div.innerText = "hello world!"
